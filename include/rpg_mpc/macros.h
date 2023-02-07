f98cb5f7a0692cccdd2d7dac8c08216454d93a9e
Hs293Go <hellston20a@gmail.com>
2023-02-07 02:48:27 -0500

    Fix some cpplint complaints

---
+++ include/rpg_mpc/macros.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 hs293go
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#pragma once
+
+#define MARK_UNUSED(arg) static_cast<void>(arg)
